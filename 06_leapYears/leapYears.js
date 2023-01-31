8465c0b9713584c7ba46e2ffcea37e1c04449575
Rich Fromm <github@richard.fromm.name>
2023-01-31 12:18:26 -0800

    Solve Exercise 06: leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // In general, the rule is divisible by 4
+    if (year % 4 != 0) {
+        return false;
+    }
+    // But even if a year *is* divisible by 4,
+    // it's *not* a leap year if it's divisible by 100
+    if (year % 100 == 0) {
+        // Unless it's also divisible by 400,
+        // then it *is* a leap year
+        if (year % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
 
+    // If we get this far, it's a normal leap year
+    return true;
 };
 
 // Do not edit below this line
