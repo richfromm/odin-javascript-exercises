98858abb91abdb49828ea39cdbc05a9180da9518
Rich Fromm <github@richard.fromm.name>
2023-01-31 11:36:47 -0800

    Switch to using rest parameters
    Which is a simpler way of dealing with varargs than the special
    'arguments' argument.
    
    https://stackoverflow.com/a/960870/9797192
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,4 @@
-const removeFromArray = function () {
-    // arguments is not actually an array
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
-    // https://stackoverflow.com/questions/960866/how-can-i-convert-the-arguments-object-to-an-array-in-javascript
-    // console.log(`arguments = ${arguments}`);
-    // console.log(`is array ${Array.isArray(arguments)}`);
-    let args = Array.from(arguments);
-    let ary = args.shift();
-    let toRemove = args;
+const removeFromArray = function (ary, ...toRemove) {
     toRemove.forEach(item =>
         ary = ary.filter(element => element !== item)
     );
