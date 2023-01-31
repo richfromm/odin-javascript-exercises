a76080c5af95e48bb52a7e888435a8078c76e346
Rich Fromm <github@richard.fromm.name>
2023-01-30 22:27:47 -0800

    Solve Exercise 02: repeatString
    this was a good learning exercise in using the debugger
    although I was a little confused at first
    see https://discord.com/channels/505093832157691914/597796701650026517/1069863431731167292

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    console.debug(`repeastString(${string}, ${num})`);
+    if (parseInt(num) == NaN || num < 0) {
+        return "ERROR";
+    }
+    let retString = "";
+    for (let i = 0; i < num; i++) {
+        retString += string;
+    }
+    return retString;
 };
 
 // Do not edit below this line
