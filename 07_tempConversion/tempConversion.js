2dcdb11a8b0f53c842a587293c0344af79ed5c83
Rich Fromm <github@richard.fromm.name>
2023-01-31 12:25:54 -0800

    Solve Exercise 07: tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function roundToTenths(float) {
+  return Math.round(float * 10) / 10;
+}
+
+const convertToCelsius = function (tempF) {
+  let tempC = (tempF - 32) * (5 / 9);
+  return roundToTenths(tempC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  let tempF = (tempC * (9 / 5)) + 32;
+  return roundToTenths(tempF);
 };
 
 // Do not edit below this line
