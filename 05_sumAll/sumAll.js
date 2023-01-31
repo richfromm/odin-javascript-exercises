f4d2239f0c0865f8a60c54fdd3a4d793c5f308ed
Rich Fromm <github@richard.fromm.name>
2023-01-31 12:08:41 -0800

    Solve Exercise 05: sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (typeof a != 'number' || typeof b != 'number') {
+        return "ERROR";
+    }
 
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    let low = Math.min(a, b);
+    let high = Math.max(a, b);
+    let sum = 0;
+    for (let n = low; n <= high; n++) {
+        sum += n;
+    }
+    return sum;
 };
 
 // Do not edit below this line
