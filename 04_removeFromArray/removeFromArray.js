00ef9b1d1b02316d5ac324096826c9908ddd9468
Rich Fromm <github@richard.fromm.name>
2023-01-31 11:25:48 -0800

    Solve Exercise 04: removeFromArray
    Used the special var arguments to solve:
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
    
    But note that while arguments is Array-like in many ways, it's not
    technically an array:
    https://stackoverflow.com/questions/960866/how-can-i-convert-the-arguments-object-to-an-array-in-javascript
    (Can test with Array.isArray())
    
    So convert it with Array.from()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+    // arguments is not actually an array
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+    // https://stackoverflow.com/questions/960866/how-can-i-convert-the-arguments-object-to-an-array-in-javascript
+    // console.log(`arguments = ${arguments}`);
+    // console.log(`is array ${Array.isArray(arguments)}`);
+    let args = Array.from(arguments);
+    let ary = args.shift();
+    let toRemove = args;
+    toRemove.forEach(item =>
+        ary = ary.filter(element => element !== item)
+    );
+    return ary;
 };
 
 // Do not edit below this line
