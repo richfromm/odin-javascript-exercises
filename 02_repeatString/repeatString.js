6d7cefaa093c5ae3ab9b41903c8cd21c6ad1844c
Rich Fromm <github@richard.fromm.name>
2023-01-31 12:44:29 -0800

    tiny debug output typo fix only

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,5 @@
 const repeatString = function(string, num) {
-    console.debug(`repeastString(${string}, ${num})`);
+    console.debug(`repeatString(${string}, ${num})`);
     if (parseInt(num) == NaN || num < 0) {
         return "ERROR";
     }
